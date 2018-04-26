be1d4c65eeb595874cf5899f832ab56ddc4b88ba
dwzd <dwzd@users.noreply.github.com>
2018-04-26 11:52:34 -0400

    Page Object about expedia, to test one way flight #6

---
+++ src/main/java/com/ddd/testng/testclasses/TestClass1.java
@@ -0,0 +1,13 @@
+package com.ddd.testng.testclasses;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+    /**
+     *
+     */
+    @Test
+    public void testMethod(){
+
+    }
+}
